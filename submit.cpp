8cf64e96ccc5f3b03a2846f72cb5e94b6c6e6520
Karl Wang <changhai_wang@umail.ucsb.edu>
2018-03-02 23:53:38 -0800

    added serial implementation of life()

--- submit.cpp
+++ submit.cpp
@@ -38,6 +38,17 @@ void readlife(int *a, unsigned int n, char *filename)
 	file.close();
 }
 
+unsigned getNeighborCount(const int *a, const unsigned int n, const unsigned int i, const unsigned int j){
+	unsigned ui = (i == 0) ? n-1 : i-1;
+	unsigned di = (i == n-1) ? 0 : i+1;
+	unsigned lj = (j == 0) ? n-1 : j-1;
+	unsigned rj = (j == n-1) ? 0 : j+1;
+
+	return a[ui*n + lj] + a[ui*n +  j] + a[ui*n + rj]
+		 + a[ i*n + lj] + 			     a[ i*n + rj]
+		 + a[di*n + lj] + a[di*n +  j] + a[di*n + rj];
+}
+
 //Life function
 void life(int *a, unsigned int n, unsigned int iter, int *livecount)
 {
@@ -62,4 +73,37 @@ void life(int *a, unsigned int n, unsigned int iter, int *livecount)
 	//		#ENDIF
 	//
 	//	}
+	
+
+
+	for(unsigned currIter = 0; currIter < iter; currIter++){
+		//use b as the result grid; calloc'ed to all 0s
+		int *b = (int *)calloc(n*n, sizeof(int));
+
+		for(unsigned i = 0; i < n; i++){
+			for(unsigned j = 0; j < n; j++){
+				unsigned neighborCount = getNeighborCount(a, n, i, j);
+
+				if( neighborCount == 3 || (neighborCount == 2 && a[i*n + j] == 1) ){
+					b[i*n + j] = 1;
+				} //otherwise it is 0, but there is no need to set it as it is calloc'ed
+			}
+		}
+
+		//a will not be used again
+		free(a);
+		//swap a and b
+		a = b;
+
+		#if DEBUG == 1
+			int period = (iter/10);					//the collection period						e.g. period = 55 / 10 = 5
+			if(iter >= 10 							//if there is at least 10 iteration			e.g. 55 >= 10 == true, 
+				&& (currIter + 1) % period == 0 	//if it is right at the end of a period		e.g. (4 + 1) % 5 == 0, (49 + 1) % 5 == 0
+				&& currIter / period < 10){			//if it is among the first 10 period		e.g. 49 / 5 < 10 == true, 54 / 5 < 10 == false
+
+				livecount[currIter / period] = countlive(a, n);
+			}
+		#endif
+
+	}
 }
